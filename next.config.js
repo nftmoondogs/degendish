e563879744f9da5552827a6f0fcfa4e85b9621b2
nftmoondogs <nftmoondogs@gmail.com>
2024-02-25 00:16:28 +0530

    Describe your changes here

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images:{
-    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com", "i.ibb.co"]
+    domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com", "i.ibb.co", "solanascoop.com"]
   }
 }
 

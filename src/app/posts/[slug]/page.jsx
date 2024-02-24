dde6219bcda3bbd588d284a0114af0982d0ec4df
nftmoondogs <nftmoondogs@gmail.com>
2024-02-25 04:23:49 +0530

    Describe your changes here

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -18,44 +18,71 @@ const getData = async (slug) => {
 
 const SinglePage = async ({ params }) => {
   const { slug } = params;
-
   const data = await getData(slug);
 
   return (
     <div className={styles.container}>
-      <div className={styles.infoContainer}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>{data?.title}</h1>
-          <div className={styles.user}>
-            {data?.user?.image && (
-              <div className={styles.userImageContainer}>
-                <Image src={data.user.image} alt="" layout="fill" objectFit="cover" />
-              </div>
+      <Head>
+        <title>{data?.title}</title>
+        {/* Add meta tags for SEO if needed */}
+      </Head>
+
+      <main className={styles.mainContent}>
+        <article className={styles.post}>
+          <header className={styles.postHeader}> 
+            <h1>{data?.title}</h1> 
+          </header>
+
+          <div className={styles.postContent}> 
+            {data?.img && (
+              <figure className={styles.imageContainer}>
+                <Image
+                  src={data.img}
+                  alt={data?.title}
+                  layout="responsive"
+                  width={700}
+                  height={450}
+                  sizes="(max-width: 768px) 100vw, 700px"
+                  loading="eager"
+                />
+              </figure>
             )}
-            <div className={styles.userTextContainer}>
-              <span className={styles.username}>{data?.user?.name}</span>
-              <span className={styles.date}>01.01.2024</span>
-            </div>
+
+            <div className={styles.user} style={{ marginTop: '15px' }}> 
+              {data?.user?.image && (
+                <figure className={styles.userImageContainer}>
+                  <Image
+                    src={data.user.image}
+                    alt={data?.user?.name}
+                    layout="fill"
+                    objectFit="cover"
+                    priority={true}
+                    loading="eager"
+                  />
+                </figure>
+              )}
+              <div className={styles.userTextContainer}>
+                <span className={styles.username}>{data?.user?.name}</span>
+                <span className={styles.date}>01.01.2024</span>
+              </div>
+            </div>  
           </div>
-        </div>
-        {data?.img && (
-          <div className={styles.imageContainer}>
-            <Image src={data.img} alt="" layout="fill" objectFit="cover" />
-          </div>
-        )}
-      </div>
-      <div className={styles.content}>
-        <div className={styles.post}>
-          <div
-            className={styles.description}
+
+          <section 
+            className={styles.description} 
+            style={{ marginTop: '30px' }} 
             dangerouslySetInnerHTML={{ __html: data?.desc || '' }}
           />
-          <div className={styles.comment}>
-            <Comments postSlug={slug}/>
+
+          <div className={styles.comment} style={{ marginTop: '30px' }}> 
+            <Comments postSlug={slug} />
           </div>
-        </div>
-        <Menu />
-      </div>
+        </article>
+
+        <aside className={styles.sidebar}>
+          <Menu />
+        </aside>
+      </main>
     </div>
   );
 };

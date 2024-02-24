c2de2802a832a24be3eae838c6393f2c3e2524fe
nftmoondogs <nftmoondogs@gmail.com>
2024-02-25 03:27:16 +0530

    Describe your changes here

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,23 +4,23 @@ import Link from "next/link";
 
 const Card = ({ key, item }) => {
   return (
-    <div className={styles.container} key={key}>
-      {item.img && (
-        <div className={styles.imageContainer}>
-          <Image src={item.img} alt="" layout="fill" objectFit="cover" className={styles.image} />
+    <Link href={`/posts/${item.slug}`}>
+      <div className={styles.container} key={key}>
+        {item.img && (
+          <div className={styles.imageContainer}>
+            <Image src={item.img} alt="" layout="fill" objectFit="cover" className={styles.image} />
+          </div>
+        )}
+        <div className={styles.textContainer}>
+          <h1 className={styles.title}>{item.title}</h1>
+          <div className={styles.detail}>
+            <span className={styles.date}>{item.createdAt.substring(0, 10)}</span>
+            <span className={styles.category}>{item.catSlug}</span>
+          </div>
+          {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
         </div>
-      )}
-      <div className={styles.textContainer}>
-        <Link href={`/posts/${item.slug}`}> 
-          <h1 className={styles.title}>{item.title}</h1> 
-        </Link>
-        <div className={styles.detail}>
-          <span className={styles.date}>{item.createdAt.substring(0, 10)}</span>
-          <span className={styles.category}>{item.catSlug}</span>
-        </div>
-       {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
       </div>
-    </div>
+    </Link>
   );
 };
 
--- src/components/featured/Card.jsx
+++ src/components/featured/Card.jsx
@@ -5,24 +5,26 @@ import Link from "next/link";
 const Card = ({ key, item }) => {
   return (
     <div className={styles.container} key={key}>
-      {item.img && (
-        <div className={styles.imageContainer}>
-          <Image src={item.img} alt="" fill className={styles.image} />
+      <Link href={`/posts/${item.slug}`}>
+        <div>
+          {item.img && (
+            <div className={styles.imageContainer}>
+              <Image src={item.img} alt="" fill className={styles.image} />
+            </div>
+          )}
+          <div className={styles.textContainer}>
+            {/* The h1 now has responsive font-size */}
+            <h1 className={styles.responsiveTitle}>{item.title}</h1>
+            {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
+            <div className={styles.detail}>
+              <span className={styles.date}>
+                {item.createdAt.substring(0, 10)} -{" "}
+              </span>
+              <span className={styles.category}>{item.catSlug}</span>
+            </div>
+          </div>
         </div>
-      )}
-      <div className={styles.textContainer}>
-        <Link href={`/posts/${item.slug}`}>
-          {/* The h1 now has responsive font-size */}
-          <h1 className={styles.responsiveTitle}>{item.title}</h1>
-        </Link>
-        {/* <p className={styles.desc}>{item.desc.substring(0, 60)}</p> */}
-        <div className={styles.detail}>
-          <span className={styles.date}>
-            {item.createdAt.substring(0, 10)} -{" "}
-          </span>
-          <span className={styles.category}>{item.catSlug}</span>
-        </div>
-      </div>
+      </Link>
     </div>
   );
 };
